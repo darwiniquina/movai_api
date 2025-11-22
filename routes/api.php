03af564effff191c3a3215e0437462fd2a77d6f1
darwiniquina <darwiniquina@gmail.com>
2025-11-22 21:38:05 +0800

    feat: implement test route for debugging

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,12 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
+Route::get('test', function () {
+    return response()->json([
+        'message' => 'Test'
+    ]);
+});
+
 Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
